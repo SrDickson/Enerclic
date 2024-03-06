bc9806311ea4790ae33cbee696e54e53859cfffd
Javier Medina Fernández <byjavi14.real@gmail.com>
2024-03-06 18:23:51 +0100

    script linux-macOS.sh

--- script linux-macOS.sh
+++ script linux-macOS.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
-# Construir la imagen del servidor
 docker build -t chat_server -f Dockerfile .
 
-# Ejecutar el contenedor del servidor en segundo plano
 docker run -d --name chat_server -p 8888:8888 chat_server
 
-# Esperar un breve momento antes de iniciar los clientes
 sleep 2
 
-# Ejecutar clientes en dos terminales de sistema diferentes
 gnome-terminal -- python client.py
 gnome-terminal -- python client.py
